93fac5fa06ad2565240826ff0ea63dbbaca40562
Hanif Ardhani <hanif.ardhani98@gmail.com>
2021-10-26 09:49:06 +0700

    sepolicy: Cleanup unnecessary stuff
    Co-authored-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
    Change-Id: I0c889f9f1617e757d739abd078d7faae94e94ab1

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,27 +3,9 @@
 # inherit from Proton
 #
 
-ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
-ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
-endif
-endif
-
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/proton/sepolicy/common/public
-
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/proton/sepolicy/common/private
 
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/proton/sepolicy/common/dynamic \
-    device/proton/sepolicy/common/system
-else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/proton/sepolicy/common/dynamic \
     device/proton/sepolicy/common/vendor
-endif
-
-# Selectively include legacy rules defined by the products
--include device/proton/sepolicy/legacy-common/sepolicy.mk
